483bcfd058bc8cfbe430bb9328ea610b037334a6
LV11coding <94009217+LV11coding@users.noreply.github.com>
2024-03-13 18:21:07 +0200

    Updated bluetooth.ino to 1.1
    New cooldown mode. New console messages.

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,34 +1,34 @@
 #include <SoftwareSerial.h>
-SoftwareSerial BTSerial (2, 3);
-
+SoftwareSerial BTSerial(2, 3);
+ 
 char target = 'A';
 int num = 0;
-
+ 
 void setup() {
   Serial.begin(9600);
   BTSerial.begin(9600);
-
+  Serial.println("Enter password:");
 }
-
-
+ 
+ 
 void loop() {
   if (BTSerial.available()) {
     //  Serial.write(BTSerial.read());
-
+ 
     if (BTSerial.read() == target) {
-      Serial.println(":0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0");
+      Serial.println("Right");
       num = 0;
-    }
-    else if (BTSerial.read() != target) {
-      Serial.println("wrong");
+    } else if (BTSerial.read() != target) {
+      Serial.println("Wrong");
       num++;
     }
     if (num > 4) {
       num = 0;
       Serial.println("Entered cooldown mode: Try again after 1 minute");
-       BTSerial.end();
-       delay(60000);
-       BTSerial.begin(9600);
+      BTSerial.end();
+      delay(60000);
+      BTSerial.begin(9600);
+      Serial.println("Exited cooldown mode: Try again");
     }
   }
 }
